eb6447eb46d9eb3f406d345d14f5ffeebf75cf14
suhasledange <suhasledange10@gmail.com>
2023-03-14 18:01:28 +0530

    backend

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const mongoURI = "mongodb://localhost:27017/notebook";
 
 const connectToMongo = ()=>{
+    
     mongoose.connect(mongoURI);
 }
 module.exports = connectToMongo;
