f9ba8f52463174aad4a3962bbd843e502ca798ef
suhasledange <suhasledange10@gmail.com>
2023-04-27 20:42:37 +0530

    popup

--- src/components/NotesBlock.js
+++ src/components/NotesBlock.js
@@ -45,7 +45,7 @@ const onChange = (e)=>{
 </button>
 
 <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog">
+  <div className="modal-dialog modal-dialog-centered">
     <div className="modal-content">
       <div className="modal-header">
         <h1 className="modal-title fs-5" id="exampleModalLabel">Update Note</h1>
